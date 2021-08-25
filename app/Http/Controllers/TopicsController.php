41bb076ba36c90af0af60507c5bec669e86596df
norie <norieli@qq.com>
2021-08-25 16:18:54 +0800

    话题列表

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,6 +6,7 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Models\Category;
 
 class TopicsController extends Controller
 {
@@ -14,15 +15,20 @@ class TopicsController extends Controller
 		$this->middleware('auth', ['except' => ['index', 'show']]);
 	}
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
-		$topics = Topic::with('user', 'category')->paginate(30);
+		$topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
-	public function show(Topic $topic)
+	public function show(Category $category, Request $request, Topic $topic)
 	{
-		return view('topics.show', compact('topic'));
+		// 读取分类 ID 关联的话题，并按每 20 条分页
+		$topics = $topic->withOrder($request->order)
+			->where('category_id', $category->id)
+			->paginate(20);
+		// 传参变量话题和分类到模板中
+		return view('topics.index', compact('topics', 'category'));
 	}
 
 	public function create(Topic $topic)
