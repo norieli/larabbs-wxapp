5da58d8ad7d8598e519505b23844e12fbd526129
norie <norieli@qq.com>
2021-08-26 17:25:23 +0800

    队列

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -47,7 +47,7 @@ class TopicsController extends Controller
 		$topic->fill($request->all());
 		$topic->user_id = Auth::id();
 		$topic->save();
-		return redirect()->route($topic->link())->with('success', '帖子创建成功！');
+		return redirect()->to($topic->link())->with('success', '帖子创建成功！');
 	}
 
 	public function edit(Topic $topic)
@@ -62,7 +62,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route($topic->link())->with('success', '更新成功！');
+		return redirect()->to($topic->link())->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
