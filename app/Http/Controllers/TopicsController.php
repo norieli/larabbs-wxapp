97058894376534db8566f895e7585a47d2178d85
norie <norieli@qq.com>
2021-08-26 13:56:50 +0800

    编辑帖子

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -17,14 +17,13 @@ class TopicsController extends Controller
 		$this->middleware('auth', ['except' => ['index', 'show']]);
 	}
 
-	public function index(Category $category, Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic)
 	{
 		// 读取分类 ID 关联的话题，并按每 20 条分页
 		$topics = $topic->withOrder($request->order)
-			->where('category_id', $category->id)
 			->paginate(20);
 		// 传参变量话题和分类到模板中
-		return view('topics.index', compact('topics', 'category'));
+		return view('topics.index', compact('topics'));
 	}
 
 	public function show(Topic $topic)
@@ -49,7 +48,8 @@ class TopicsController extends Controller
 	public function edit(Topic $topic)
 	{
 		$this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+		$categories = Category::all();
+		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
@@ -57,7 +57,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
@@ -65,7 +65,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '删除成功');
 	}
 
 	public function uploadImage(Request $request, ImageUploadHandler $uploader)
