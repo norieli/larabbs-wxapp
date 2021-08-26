945d3a33f9417bf8db3190682b473d4a6a1fd3ac
norie <norieli@qq.com>
2021-08-26 16:05:34 +0800

    url优化翻译

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -22,7 +23,12 @@ class TopicObserver
     public function saving(Topic $topic)
     {
 
-        $topic->body = clean2($topic->body, 'user_topic_body');
+        $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if (!$topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
