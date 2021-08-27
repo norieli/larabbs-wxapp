6a29f7f1c6a726d7cffeac3fa32739c406e5d815
norie <norieli@qq.com>
2021-08-27 15:24:06 +0800

    权限部署

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -37,5 +37,11 @@ class UsersTableSeeder extends Seeder
         $user->email = 'admin@163.com';
         $user->avatar = 'https://gravatar.loli.top/avatar/cfedc000762cd0bd150ad1259119ac52?s=32';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
