3cd37fb3a6565b94c41c129e9d774510b4f6dde3
norie <norieli@qq.com>
2021-08-27 17:43:05 +0800

    后台管理

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use HasRoles; 
+    use HasRoles;
     use MustVerifyEmailTrait;
     use Notifiable {
         notify as protected laravelNotify;
@@ -77,5 +77,25 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if (!starts_with($path, 'http')) {
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+        $this->attributes['avatar'] = $path;
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+        $this->attributes['password'] = $value;
     }
 }
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">由 <a href="/" target="_blank">Norie</a> 设计和编码  </p>
-        <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
