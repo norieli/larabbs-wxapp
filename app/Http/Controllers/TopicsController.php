a6cc3858cc34d25793daa220d0b5f68338703057
norie <norieli@qq.com>
2021-08-31 10:19:01 +0800

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class TopicsController extends Controller
@@ -17,13 +18,11 @@ class TopicsController extends Controller
 		$this->middleware('auth', ['except' => ['index', 'show']]);
 	}
 
-	public function index(Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic, User $user)
 	{
-		// 读取分类 ID 关联的话题，并按每 20 条分页
-		$topics = $topic->withOrder($request->order)
-			->paginate(20);
-		// 传参变量话题和分类到模板中
-		return view('topics.index', compact('topics'));
+		$topics = $topic->withOrder($request->order)->paginate(20);
+		$active_users = $user->getActiveUsers();
+		return view('topics.index', compact('topics', 'active_users'));
 	}
 
 	public function show(Request $request, Topic $topic)
--- resources/views/pages/permission_denied.blade.php
+++ resources/views/pages/permission_denied.blade.php
@@ -20,4 +20,4 @@
         </div>
     </div>
 </div>
-@sto
+@stop
