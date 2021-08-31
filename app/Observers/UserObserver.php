1a4fa92f2900ebf60efd1c8dfa0d9c9248f0174d
norie <norieli@qq.com>
2021-08-31 14:06:02 +0800

    首页、默认头像、样式

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,12 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://gravatar.loli.top/avatar/cfedc000762cd0bd150ad1259119ac52?s=32';
+        }
+    }
 }
